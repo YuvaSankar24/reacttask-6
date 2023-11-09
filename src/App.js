4a6251aaa0f860395b410e066bed655946d34271
unknown <yuvasankarpotluru@gmail.comgit config --global user.name YuvaSankar24>
2023-11-09 12:31:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header className="header">
+        <h1>Uday Sai Manoj</h1>
       </header>
+
+      <div className="content">
+        <section className="section">
+          <h2>Introduction</h2>
+          <p>
+            I'm Yuva Sankar, a B.Tech student,from VIT-AP
+          </p>
+        </section>
+
+        <section className="section">
+          <h2>Skills</h2>
+          <ul>
+            <li>Java</li>
+            <li>Python</li>
+            <li>Data Structures</li>
+            <li>HTML,CSS,JavaScript,React.js,Node.js</li>
+            <li>SQL,MongoDb</li>
+          </ul>
+        </section>
+
+        <section className="section">
+          <h2>Projects</h2>
+          <ul>
+            <li>Disease Diagnosis Through Multi-Modal Data Integration and Data Weighting Using Deep Learning</li>
+            <li>CrimeBot: Crime Registration and Awareness Web Application</li>
+          </ul>
+        </section>
+        <section className="section">
+          <h2>Certifications</h2>
+          <ul>
+            <li>AWS Academy Graduate - AWS Academy Cloud Foundations</li>
+            <li>React - The Complete Guide 2023 (incl. React Router & Redux)</li>
+          </ul>
+        </section>
+        
+
+        <section className="section">
+          <h2>Contact</h2>
+          <a href="mailto:udaysaimanoj14196@gmail.com">Email me</a>
+        </section>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; // Add this export statement to make the component available for use in other parts of your application
